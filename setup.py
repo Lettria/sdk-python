438a07f9f39bc0e00b537e2754882899500b236f
vic <vde-la-s@student.42.fr>
2020-10-15 15:07:40 +0200

    update long_description

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="lettria",
-    version="5.0.0",
+    version="5.0.1",
     packages=find_packages(),
     description="Lettria official SDK for python",
+    long_description="Lettria official SDK for python",
     author="Maxence",
     author_email="maxence@lettria.com",
     url="https://github.com/Lettria/sdk-python",
